8fb500b934f9e72f1405abd30fc0e6ac869bdc6b
Nipuna Weerasekara <w.nipuna@gmail.com>
2019-05-13 16:26:15 +0530

    Major updates
    Added the select user type functionality

---
+++ assets/js/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyA1SGhU7-cunQ7VIrCmAVWSYHfe9-FLb6w",
+    authDomain: "foodanalyser.firebaseapp.com",
+    databaseURL: "https://foodanalyser.firebaseio.com",
+    projectId: "foodanalyser",
+    storageBucket: "foodanalyser.appspot.com",
+    messagingSenderId: "67786128135",
+    appId: "1:67786128135:web:c5de525f78d13905"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
