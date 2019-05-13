81b6bb089e2f73895ea15dec6b314eb927aebe4d
Nipuna Weerasekara <w.nipuna@gmail.com>
2019-05-14 03:11:53 +0530

    Major updates
    Added the functionality to search for a E ingredients across Beverages collection and Dessert collection

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -22,6 +22,7 @@ function searchENumbers() {
 
   // This points to the collection called 'Dessert'
   var collectionReference = db.collection('Dessert');
+  var collectionReferenceBeverages = db.collection('Beverages');
   
   var ins = document.getElementById("ins").value;
 
@@ -29,40 +30,77 @@ function searchENumbers() {
     alert("Please enter a valid ingredient! Example: E321")
   }else{
     var query = collectionReference.where("pv", "array-contains", ins);
+    var queryBeverages = collectionReferenceBeverages.where("pv", "array-contains", ins);
     
+    var tableTemplate = '<table class="table table-striped table-dark" id="dataTable"><thead><tr><th scope="col">Name</th><th scope="col">Preservatives</th></tr></thead><tbody>';
+    var tableBody = '';
+
+    var bigArray = [];
+    var checkDocsOne = 0;
+    var checkDocsTwo = 0;
+
     query.get().then(function(querySnapshot) {
       // check and do something with the data here.
       if (querySnapshot.empty) {
-        document.getElementById('dataTableDiv').innerHTML = '<p class="h4 text-center">Sorry No Results to Display!</p>';
+        checkDocsOne = 0;
       } else {
         // do something with the data
         // go through all the results
-      var tableTemplate = '<table class="table table-striped table-dark" id="dataTable"><thead><tr><th scope="col">Name</th><th scope="col">Preservatives</th></tr></thead><tbody>';
-      var tableBody = '';
-      querySnapshot.forEach(function (documentSnapshot) {
-      var data = documentSnapshot.data();
-      // do something with the data of each document.
-      name = JSON.stringify(data.name);
-      preservatives = data.pv;
-
-      var pvString = '';
-      preservatives.forEach(function(item){
+        checkDocsOne = 1;
+      
+        querySnapshot.forEach(function (documentSnapshot) {
+        var data = documentSnapshot.data();
+        // do something with the data of each document.
+          bigArray.push(data);
+        });
         
-        if (item != ins){
-          pvString += '<span>' + item + '&nbsp; &nbsp; &nbsp; </span>';
-        }else{
-          pvString += '<span style="color:red">' + item + '&nbsp; &nbsp; &nbsp; </span>';
-        }
-        
-      });
-
-      tableBody += '<tr><td>' + JSON.parse(name) + '</td><td>' + pvString + '</td></tr>';
-      });
-      var tableFooter = '  </tbody></table>';
-      document.getElementById('dataTableDiv').innerHTML = tableTemplate+tableBody+tableFooter;
       }
     });
+    
+
+    queryBeverages.get().then(function(querySnapshot) {
+      // check and do something with the data here.
+      if (querySnapshot.empty) {
+        checkDocsTwo = 0;
+      } else {
+        // do something with the data
+        // go through all the results
+        checkDocsTwo = 1;
+      
+        querySnapshot.forEach(function (documentSnapshot) {
+        var data = documentSnapshot.data();
+        // do something with the data of each document.
+          bigArray.push(data);
+        });
+        
+      }
+    });
+    
+    setTimeout(function(){
+      if ((checkDocsOne + checkDocsTwo) == 1 || (checkDocsOne + checkDocsTwo) == 2 ){
+        bigArray.forEach(function (item) {
+        name = JSON.stringify(item.name);
+        preservatives = item.pv;
+
+        var pvString = '';
+        preservatives.forEach(function(item){
+          
+          if (item != ins){
+            pvString += '<span>' + item + '&nbsp; &nbsp; &nbsp; </span>';
+          }else{
+            pvString += '<span style="color:red">' + item + '&nbsp; &nbsp; &nbsp; </span>';
+          }
+          
+        });
+          tableBody += '<tr><td>' + JSON.parse(name) + '</td><td>' + pvString + '</td></tr>';
+        });
+        var tableFooter = '</tbody></table>';
+        document.getElementById('dataTableDiv').innerHTML = tableTemplate+tableBody+tableFooter;
+      }else{
+        document.getElementById('dataTableDiv').innerHTML = '<p class="h4 text-center">Sorry No Results to Display!</p>';
+      }
+    }, 1000);
+
   }
 }
 //end of js file to the allergic_details.html file
-//double check
